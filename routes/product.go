c70d4767d6fb91057d44685738239c39ada161e8
Fábio Vilalba <fabiovilalba@Fabios-MacBook-Pro.local>
2023-07-02 17:05:08 -0300

    Create another REST for products

--- routes/product.go
+++ routes/product.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"errors"
+
 	"github.com/fabiovalinhos/fiber-api/database"
 	"github.com/fabiovalinhos/fiber-api/models"
 	"github.com/gofiber/fiber/v2"
@@ -20,6 +22,15 @@ func CreateResponseProduct(productModel models.Product) Product {
 	}
 }
 
+func findProduct(id int, product *models.Product) error {
+	database.Database.Db.Find(&product, "id = ?", id)
+	if product.ID == 0 {
+		return errors.New("product does not exist")
+	}
+
+	return nil
+}
+
 func CreateProduct(c *fiber.Ctx) error {
 	var product models.Product
 
@@ -32,4 +43,82 @@ func CreateProduct(c *fiber.Ctx) error {
 	responseProduct := CreateResponseProduct(product)
 
 	return c.Status(200).JSON(responseProduct)
+}
+
+func GetProducts(c *fiber.Ctx) error {
+	products := []models.Product{}
+
+	database.Database.Db.Find(&products)
+	responseProducts := []Product{}
+	for _, product := range products {
+		responseProduct := CreateResponseProduct(product)
+		responseProducts = append(responseProducts, responseProduct)
+	}
+
+	return c.Status(200).JSON(responseProducts)
+}
+
+func GetProduct(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return c.Status(400).JSON("Please ensure that :id is an integer")
+	}
+
+	var product models.Product
+	if err := findProduct(id, &product); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	responseProduct := CreateResponseProduct(product)
+
+	return c.Status(200).JSON(responseProduct)
+}
+
+func UpdateProduct(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return c.Status(400).JSON("Please ensure that :id is an integer")
+	}
+
+	var product models.Product
+	if err := findProduct(id, &product); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	type UpdateProduct struct {
+		Name         string `json:"name"`
+		SerialNumber string `json:"serial_number"`
+	}
+
+	var updateData UpdateProduct
+
+	if err := c.BodyParser(&updateData); err != nil {
+		return c.Status(500).JSON(err.Error())
+	}
+
+	product.Name = updateData.Name
+	product.SerialNumber = updateData.SerialNumber
+
+	database.Database.Db.Save(&product)
+	responseProduct := CreateResponseProduct(product)
+
+	return c.Status(200).JSON(responseProduct)
+}
+
+func DeleteProduct(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return c.Status(400).JSON("Please ensure that :id is an integer")
+	}
+
+	var product models.Product
+	if err := findProduct(id, &product); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	if err := database.Database.Db.Delete(&product).Error; err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+
+	return c.Status(200).SendString("successfully Deleted User")
 }
